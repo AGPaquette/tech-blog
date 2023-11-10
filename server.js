2ab0c138e5786cd3fe5770d8292bb701067496e0
AGPaquette <131227245+AGPaquette@users.noreply.github.com>
2023-11-09 20:25:35 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');
+// Load environment variables (for local development)
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// Use environment variables for database configuration
+const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_DB_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Session configuration
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESSION_SECRET || 'Super secret secret',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
@@ -21,23 +33,31 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
+// Handlebars configuration
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware and static assets
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use(require('./controllers/'));
 
+// Start the server
 app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  
+  // Sync Sequelize models with the database
   sequelize.sync({ force: false });
 });
+
